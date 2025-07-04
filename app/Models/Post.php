0e675c9e462ff4547727876bf5a4bbee260fc939
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-04 23:40:37 +0545

    feat: category crud & model migration

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'thumbnail',
+        'title',
+        'color',
+        'slug',
+        'category_id',
+        'content',
+        'tags',
+        'published',
+    ];
+}
