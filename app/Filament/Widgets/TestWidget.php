22cfd713e9d28901d83f92226692446cdb6bb009
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-12 17:40:42 +0545

    feat: custon stat widget filament

---
+++ app/Filament/Widgets/TestWidget.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class TestWidget extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('New Users',User::count())
+        ];
+    }
+}
