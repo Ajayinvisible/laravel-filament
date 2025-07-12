645e3f6f4c974bbc21a78851e9cfc28a44b8983e
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-12 18:09:54 +0545

    feat: widget description, icon & graph in stat

--- app/Filament/Widgets/TestWidget.php
+++ app/Filament/Widgets/TestWidget.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\User;
+use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -12,6 +13,10 @@ class TestWidget extends BaseWidget
     {
         return [
             Stat::make('New Users',User::count())
+            ->description('New users that have joined')
+            ->descriptionIcon('heroicon-s-user-group',IconPosition::Before)
+            ->chart([10,15,5,35,11,66,22,100])
+            ->color('success')
         ];
     }
 }
