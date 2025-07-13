e3212f4805439fca146c8bed739f4b763cd4f636
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-13 22:53:05 +0545

    feat: widget in create and edit page resource

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Filament\Widgets\TestWidget;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -14,6 +15,13 @@ class EditUser extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            TestWidget::class
         ];
     }
 }
